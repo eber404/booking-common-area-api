b6047a7ad5b0d677cc1f63a4fb27a9e67936d390
eber404 <bodacamargo@gmail.com>
2022-01-24 22:15:13 -0300

    fix: paths for js build

---
+++ src/fixTsPaths.ts
@@ -0,0 +1,8 @@
+import ModuleAlias from 'module-alias'
+
+ModuleAlias.addAliases({
+  '@/common': __dirname + '/common',
+  '@/commonArea': __dirname + '/commonArea',
+  '@/location': __dirname + '/location',
+  '@/user': __dirname + '/user',
+})
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import './fixTsPaths'
+
 import express from 'express'
 import { routes } from './routes'
 
