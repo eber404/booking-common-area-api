4d0d23cbef05e63b4adc125e03f779790a8a73be
eber404 <bodacamargo@gmail.com>
2022-01-24 22:22:17 -0300

    fix: remove unnused params

--- src/user/features/GetUsers/GetUserController.ts
+++ src/user/features/GetUsers/GetUserController.ts
@@ -5,7 +5,7 @@ import { GetUsersUseCase } from './GetUserUseCase'
 export class GetUserController {
   constructor(private readonly getUserUseCase: GetUsersUseCase) {}
 
-  async handle(params: any, query: any) {
+  async handle(params: any) {
     const id = params.id
 
     if (!id) throw BadRequest('Id inválido')
